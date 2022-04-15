2128662c37547c10f2af77f1d8fe8f0c6283ccbe
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-15 14:37:08 +0600

    header navbar section is added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import logo from '../../Images/logo/logo2.png';
+import './Header.css';
 const Header = () => {
     return (
         <div>
-            <h1>this is navbar </h1>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home"><img className='logo-img' src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#features">Cart</Nav.Link>
+                            <Nav.Link href="#pricing">Log In</Nav.Link>
+                            <Nav.Link href="#pricing" className='btn btn-danger rounded-pill px-4 text-light'>Sign Up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         </div>
     );
 };
